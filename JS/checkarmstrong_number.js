dc85acf28153a528cc322f6aeafac5553f92c145
Shubhang Upadhyay <shubhupadhyay213@gmail.com>
2026-05-11 10:58:18 +0530

    sum of digit

--- JS/checkarmstrong_number.js
+++ JS/checkarmstrong_number.js
@@ -1,3 +1,9 @@
 function solve(...args){
     const num = args[0];
-}
+    const strNum=num.toString();
+    const power=strNum.length;
+
+    let sum=0;
+    for(let digit of strNum){
+        sum+=Math.pow(Number(digit),power);
+    }
