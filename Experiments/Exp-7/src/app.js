c4c365f9e6cb5f4bc694aa0e28d5ea9dfa208c3e
Shubhang Upadhyay <shubhupadhyay213@gmail.com>
2026-05-28 17:39:06 +0530

    exp 7

---
+++ Experiments/Exp-7/src/app.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./App.css";
+
+// Student Component
+function Student(props) {
+  return (
+    <div className="student-card">
+      <h2>{props.name}</h2>
+      <p>
+        <strong>Course:</strong> {props.course}
+      </p>
+      <p>
+        <strong>Marks:</strong> {props.marks}
+      </p>
+    </div>
+  );
+}
+t
+function App() {
+  return (
+    <div className="container">
+      <h1>Student Information</h1>
+
+      <Student
+        name="Rahul Sharma"
+        course="Computer Science"
+        marks="85"
+      />
+
+      <Student
+        name="Anita Verma"
+        course="Information Technology"
+        marks="92"
+      />
+
+      <Student
+        name="Rohan Gupta"
+        course="Electronics"
+        marks="78"
+      />
+    </div>
+  );
+}
+
+export default App
